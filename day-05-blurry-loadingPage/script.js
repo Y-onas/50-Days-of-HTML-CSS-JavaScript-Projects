5345b020a28be5f1cfc0a68aa4654dfa0d491534
Y-onas <164673912+Y-onas@users.noreply.github.com>
2025-05-26 20:57:37 +0300

    day-05

---
+++ day-05-blurry-loadingPage/script.js
@@ -0,0 +1,18 @@
+const loadText = document.querySelector(".loading-text");
+const bg = document.querySelector(".bg");
+
+let load = 0;
+let int = setInterval(blurring, 30);
+
+function blurring() {
+  load++;
+  if (load > 99) {
+    clearInterval(int);
+  }
+  loadText.innerHTML = `${load}%`;
+  loadText.style.opacity = scale(load, 0, 100, 1, 0);
+  bg.style.filter = `blur(${scale(load, 0, 100, 30, 0)}px)`;
+}
+function scale(number, inmin, inmax, outmin, outmax) {
+  return ((number - inmin) * (outmax - outmin)) / (inmax - inmin) + outmin;
+}
